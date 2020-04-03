7829c137fbcbb5643e2e0a3da07f0ac4c8df86da
Nicolas Carrier <nicolas.carrier@orolia.com>
2020-04-03 07:03:59 +0200

    fixed not enough values to unpack (expected 2, got 0) error

--- discus.py
+++ discus.py
@@ -268,8 +268,8 @@ Options are:
 def parse_options():
     """Read the user's options and integrate them with the defaults."""
     try:
-        options = getopt.getopt(sys.argv[1:], "p:tgmksdcrvh",
-                                ["help", "version"])
+        options, _ = getopt.getopt(sys.argv[1:], "p:tgmksdcrvh",
+                                   ["help", "version"])
     except Exception:
         sys.exit(EX_USAGE)
 
